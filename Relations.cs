45b4d8765044e8c3f734767ca65a32681d5919c7
Doruk Demirci <dorukdemirci.boun@gmail.com>
2019-01-15 00:38:06 +0300

    First commit with Relations class

---
+++ Relations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortestPathAlgorithms
+{
+    public class Relations
+    {
+        public char source;
+        public char target;
+        public int distance;
+
+        public Relations(char source, char target, int distance)
+        {
+            this.source = source;
+            this.target = target;
+            this.distance = distance;
+        }
+    }
+}
